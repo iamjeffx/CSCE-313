2793c6b1eba03a98b8d1ec1cccc860a12376d6a1
iamjeffx <jeffreyxu@tamu.edu>
2020-08-26 16:46:15 -0400

    PA1 starter code added

---
+++ Projects/PA1/cmake-build-debug/CMakeFiles/PA1.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/osboxes/CLionProjects/CSCE313/Projects/PA1/Client.cpp" "/home/osboxes/CLionProjects/CSCE313/Projects/PA1/cmake-build-debug/CMakeFiles/PA1.dir/Client.cpp.o"
+  "/home/osboxes/CLionProjects/CSCE313/Projects/PA1/FIFOreqchannel.cpp" "/home/osboxes/CLionProjects/CSCE313/Projects/PA1/cmake-build-debug/CMakeFiles/PA1.dir/FIFOreqchannel.cpp.o"
+  "/home/osboxes/CLionProjects/CSCE313/Projects/PA1/common.cpp" "/home/osboxes/CLionProjects/CSCE313/Projects/PA1/cmake-build-debug/CMakeFiles/PA1.dir/common.cpp.o"
+  "/home/osboxes/CLionProjects/CSCE313/Projects/PA1/server.cpp" "/home/osboxes/CLionProjects/CSCE313/Projects/PA1/cmake-build-debug/CMakeFiles/PA1.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Projects/PA1/cmake-build-debug/CMakeFiles/PA1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PA1.dir/Client.cpp.o"
+  "CMakeFiles/PA1.dir/FIFOreqchannel.cpp.o"
+  "CMakeFiles/PA1.dir/common.cpp.o"
+  "CMakeFiles/PA1.dir/server.cpp.o"
+  "PA1"
+  "PA1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PA1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
