846f0095d60f3e78e4c0f7c9235bf99d7d7494cb
iamjeffx <jeffreyxu@tamu.edu>
2020-08-27 22:43:17 -0400

    1000 requests for each patient completed

--- Projects/PA1/client.cpp
+++ Projects/PA1/client.cpp
@@ -9,15 +9,34 @@
 
 using namespace std;
 
-
 int main(int argc, char *argv[]){
     FIFORequestChannel chan ("control", FIFORequestChannel::CLIENT_SIDE);
 
     // sending a non-sense message, you need to change this
-    char buf [MAX_MESSAGE];
-    char x = 55;
-    chan.cwrite (&x, sizeof (x));
-    int nbytes = chan.cread (buf, MAX_MESSAGE);
+    // request 1000 points from each patient
+    ofstream outfile("./received/x1.csv");
+    time_t start, end;
+    time(&start);
+    for(int i = 1; i <= 15; i++) {
+        for(int j = 0; j < 1000; j++) {
+            datamsg d1(i, j * 0.004, 1);
+            datamsg d2(i, j * 0.004, 2);
+
+            chan.cwrite(&d1, sizeof(datamsg));
+            double r1;
+            chan.cread(&r1, sizeof(double));
+
+            chan.cwrite(&d2, sizeof(datamsg));
+            double r2;
+            chan.cread(&r2, sizeof(double));
+
+            outfile << "Patient " << i << ", time: " << j * 0.004 << "; ecg1: " << r1 << ", ecg2: " << r2 << endl;
+        }
+    }
+    time(&end);
+    double timeTaken = double(end - start);
+    cout << "Execution time was " << timeTaken << " sec" << endl;
+    outfile.close();
     // closing the channel
     MESSAGE_TYPE m = QUIT_MSG;
     chan.cwrite (&m, sizeof (MESSAGE_TYPE));
