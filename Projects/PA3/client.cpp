e1a51080eb80826e5ca2544505f59dc69f8506a0
iamjeffx <jeffreyxu@tamu.edu>
2020-09-20 11:50:51 -0400

    Client code updated to correct version

--- Projects/PA3/client.cpp
+++ Projects/PA3/client.cpp
@@ -1,136 +1,205 @@
-#include "common.h"
+/** PA1
+ * Jeffrey Xu
+ * 09/06/2020
+ * jeffreyxu@tamu.edu
+ *
+ * Client-side of server
+ */
 #include <sys/wait.h>
+#include "common.h"
 #include "FIFOreqchannel.h"
 
 using namespace std;
 
+void getnDataPoints(FIFORequestChannel &chan, int n, int patient, int ecg, double time) {
+    // Time is specified
+    if(time != -1) {
+        datamsg msg1(patient, time, 1);
+        datamsg msg2(patient, time, 2);
+        double r1, r2;
+        chan.cwrite(&msg1, sizeof(datamsg));
+        chan.cread(&r1, sizeof(double));
+        chan.cwrite(&msg2, sizeof(datamsg));
+        chan.cread(&r2, sizeof(double));
+        if(ecg == -1) {
+            cout << time << "," << r1 << "," << r2 << endl;
+        } else if(ecg == 1) {
+            cout << time << "," << r1 << endl;
+        } else if(ecg == 2) {
+            cout << time << "," << r2 << endl;
+        }
+        return;
+    }
+
+    // Request n points from a patient
+    ofstream outfile("./received/x1.csv");
+    timeval start, end;
+    gettimeofday(&start, NULL);
+    for(int j = 0; j < n; j++) {
+        datamsg d1(patient, j * 0.004, 1);
+        datamsg d2(patient, j * 0.004, 2);
+        double r1, r2;
+
+        if(ecg == -1) {
+            chan.cwrite(&d1, sizeof(datamsg));
+            chan.cread(&r1, sizeof(double));
+
+            chan.cwrite(&d2, sizeof(datamsg));
+            chan.cread(&r2, sizeof(double));
+
+            outfile << r1 << "," << r2 << endl;
+        } else if(ecg == 1){
+            chan.cwrite(&d1, sizeof(datamsg));
+            chan.cread(&r1, sizeof(double));
+            outfile << r1 << endl;
+        } else if(ecg == 2) {
+            chan.cwrite(&d2, sizeof(datamsg));
+            chan.cread(&r2, sizeof(double));
+            outfile << r2 << endl;
+        }
+    }
+    gettimeofday(&end, NULL);
+    double time_taken = (end.tv_sec - start.tv_sec) * 1e6;
+    time_taken = (time_taken + (end.tv_usec - start.tv_usec)) * 1e-6;
+    cout << "Execution time was " << time_taken << " sec" << endl;
+    outfile.close();
+}
+
+void getDataFromFile(FIFORequestChannel &chan, string file, int capacity) {
+    ofstream outfile("./received/" + file, ios::out | ios::binary);
+    timeval start, end;
+
+    filemsg f(0, 0);
+    char* buf = new char[sizeof(filemsg) + file.size() + 1];
+    memcpy(buf, &f, sizeof(filemsg));
+    memcpy(buf + sizeof(filemsg), file.c_str(), file.size() + 1);
+    chan.cwrite(buf, sizeof(filemsg) + file.size() + 1);
+
+    __int64_t fileLength;
+    __int64_t offset = 0;
+    int length;
+    chan.cread(&fileLength, sizeof(__int64_t));
+
+    cout << "Length of file: " << fileLength << endl;
+
+    char* databuf = new char[capacity];
+    gettimeofday(&start, NULL);
+    while(offset < fileLength) {
+        if(fileLength - offset >= capacity) {
+            length = capacity;
+        } else {
+            length = fileLength - offset;
+        }
+        filemsg d(offset, length);
+        memcpy(buf, &d, sizeof(filemsg));
+        memcpy(buf + sizeof(filemsg), file.c_str(), file.size() + 1);
+        chan.cwrite(buf, sizeof(filemsg) + file.size() + 1);
+        chan.cread(databuf, length);
+        outfile.write(databuf, length);
+        offset += length;
+    }
+    gettimeofday(&end, NULL);
+
+    double time_taken = (end.tv_sec - start.tv_sec) * 1e6;
+    time_taken = (time_taken + (end.tv_usec - start.tv_usec)) * 1e-6;
+    cout << "Execution time was " << time_taken << " sec" << endl;
+
+    delete databuf;
+    delete buf;
+    outfile.close();
+}
 
 int main(int argc, char *argv[]){
+    // Flag values
+    int patient = -1;
+    double time = -1;
+    int ecg = -1;
+    char* cap = "";
+    int capacity = MAX_MESSAGE;
+    string file = "";
+    bool newChan = false;
+    MESSAGE_TYPE c = NEWCHANNEL_MSG;
+    stringstream str;
+    int opt;
 
-    int c;
-    int buffercap = MAX_MESSAGE;
-    int p = 0, ecg = 1;
-    double t = -1.0;
-    bool isnewchan = false;
-    bool isfiletransfer = false;
-    string filename;
-
-
-    while ((c = getopt (argc, argv, "p:t:e:m:f:c")) != -1){
-        switch (c){
+    // Get all args
+    while ((opt = getopt(argc, argv, "m:p:t:f:e:c")) != -1) {
+        switch (opt) {
+            case 'm':
+                cap = optarg;
+                break;
             case 'p':
-                p = atoi (optarg);
+                patient = atoi(optarg);
                 break;
             case 't':
-                t = atof (optarg);
-                break;
-            case 'e':
-                ecg = atoi (optarg);
-                break;
-            case 'm':
-                buffercap = atoi (optarg);
-                break;
-            case 'c':
-                isnewchan = true;
+                time = atof(optarg);
                 break;
             case 'f':
-                isfiletransfer = true;
-                filename = optarg;
+                file = optarg;
+                break;
+            case 'e':
+                ecg = atoi(optarg);
+                break;
+            case 'c':
+                newChan = true;
                 break;
         }
     }
 
-    // fork part
-    if (fork()==0){ // child
-
-        char* args [] = {"./server", "-m", (char *) to_string(buffercap).c_str(), NULL};
-        if (execvp (args [0], args) < 0){
-            perror ("exec filed");
-            exit (0);
+    // Child process to execute server
+    int pid = fork();
+    if(pid == 0) {
+        cout << "Child process" << endl;
+        char* args[4];
+        args[0] = "./server";
+        if(cap != "") {
+            args[1] = "-m";
+            args[2] = cap;
+            args[3] = NULL;
+        } else {
+            args[1] = NULL;
         }
-    }
+        execvp("./server", args);
+    } else {
+        cout << "Parent process" << endl;
+        FIFORequestChannel chan("control", FIFORequestChannel::CLIENT_SIDE);
 
-    FIFORequestChannel* control_chan = new FIFORequestChannel ("control", FIFORequestChannel::CLIENT_SIDE);
-    FIFORequestChannel* chan = control_chan;
-    if (isnewchan){
-        cout << "Using the new channel everything following" << endl;
-        MESSAGE_TYPE m = NEWCHANNEL_MSG;
-        control_chan->cwrite (&m, sizeof (m));
-        char newchanname [100];
-        control_chan->cread (newchanname, sizeof (newchanname));
-        chan = new FIFORequestChannel (newchanname, FIFORequestChannel::CLIENT_SIDE);
-        cout << "New channel by the name " << newchanname << " is created" << endl;
-        cout << "All further communication will happen through it instead of the main channel" << endl;
-    }
+        // Case handler for each flag
+        double r;
+        if(cap != "") {
+            capacity = atoi(cap);
+        }
+        if(newChan) {
+            chan.cwrite(&c, sizeof(MESSAGE_TYPE));
+            char *newc = new char[MAX_MESSAGE];
+            chan.cread(newc, MAX_MESSAGE);
+            FIFORequestChannel chan2(newc, FIFORequestChannel::CLIENT_SIDE);
+            cout << "Channel 2 created" << endl;
 
-
-
-    if (!isfiletransfer){   // requesting data msgs
-        if (t >= 0){    // 1 data point
-            datamsg d (p, t, ecg);
-            chan->cwrite (&d, sizeof (d));
-            double ecgvalue;
-            chan->cread (&ecgvalue, sizeof (double));
-            cout << "Ecg " << ecg << " value for patient "<< p << " at time " << t << " is: " << ecgvalue << endl;
-            return 0;
-        }else{          // bulk (i.e., 1K) data requests
-            double ts = 0;
-            datamsg d (p, ts, ecg);
-            double ecgvalue;
-            for (int i=0; i<1000; i++){
-                chan->cwrite (&d, sizeof (d));
-                chan->cread (&ecgvalue, sizeof (double));
-                d.seconds += 0.004; //increment the timestamp by 4ms
-                cout << ecgvalue << endl;
+            if(patient != -1) {
+                getnDataPoints(chan2, 1000, patient, ecg, time);
             }
-            return 0;
+            if(file != "") {
+                getDataFromFile(chan2, file, capacity);
+            }
+
+            MESSAGE_TYPE m1 = QUIT_MSG;
+            chan2.cwrite(&m1, sizeof(MESSAGE_TYPE));
+            cout << "Channel 2 closed" << endl;
+        } else {
+            if(patient != -1) {
+                getnDataPoints(chan, 1000, patient, ecg, time);
+            }
+            if(file != "") {
+                getDataFromFile(chan, file, capacity);
+            }
         }
+
+        cout << "Everything finished" << endl;
+        // closing the channel
+        MESSAGE_TYPE m = QUIT_MSG;
+        chan.cwrite(&m, sizeof (MESSAGE_TYPE));
+        cout << "Channel closed" << endl;
+        wait(0);
     }
-    else if (isfiletransfer){
-        // part 2 requesting a file
-        filemsg f (0,0);  // special first message to get file size
-        int to_alloc = sizeof (filemsg) + filename.size() + 1; // extra byte for NULL
-        char* buf = new char [to_alloc];
-        memcpy (buf, &f, sizeof(filemsg));
-        strcpy (buf + sizeof (filemsg), filename.c_str());
-        chan->cwrite (buf, to_alloc);
-        __int64_t filesize;
-        chan->cread (&filesize, sizeof (__int64_t));
-        cout << "File size: " << filesize << endl;
-
-        //int transfers = ceil (1.0 * filesize / MAX_MESSAGE);
-        filemsg* fm = (filemsg*) buf;
-        __int64_t rem = filesize;
-        string outfilepath = string("received/") + filename;
-        FILE* outfile = fopen (outfilepath.c_str(), "wb");
-        fm->offset = 0;
-
-        char* recv_buffer = new char [MAX_MESSAGE];
-        while (rem>0){
-            fm->length = (int) min (rem, (__int64_t) MAX_MESSAGE);
-            chan->cwrite (buf, to_alloc);
-            chan->cread (recv_buffer, MAX_MESSAGE);
-            fwrite (recv_buffer, 1, fm->length, outfile);
-            rem -= fm->length;
-            fm->offset += fm->length;
-            //cout << fm->offset << endl;
-        }
-        fclose (outfile);
-        delete recv_buffer;
-        delete buf;
-        cout << "File transfer completed" << endl;
-    }
-
-
-    MESSAGE_TYPE q = QUIT_MSG;
-    chan->cwrite (&q, sizeof (MESSAGE_TYPE));
-
-    if (chan != control_chan){ // this means that the user requested a new channel, so the control_channel must be destroyed as well
-        control_chan->cwrite (&q, sizeof (MESSAGE_TYPE));
-    }
-    // wait for the child process running server
-    // this will allow the server to properly do clean up
-    // if wait is not used, the server may sometimes crash
-    wait (0);
-
 }
