2fff8250975c66261fe98b459c04adb8cb6038eb
osboxes.org <jeffreyxu@tamu.edu>
2020-10-11 00:49:42 -0400

    PA4 starter code added

---
+++ Projects/PA4/cmake-build-debug/CMakeFiles/PA4.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/osboxes/Desktop/CSCE-313/Projects/PA4/FIFOreqchannel.cpp" "/home/osboxes/Desktop/CSCE-313/Projects/PA4/cmake-build-debug/CMakeFiles/PA4.dir/FIFOreqchannel.cpp.o"
+  "/home/osboxes/Desktop/CSCE-313/Projects/PA4/Histogram.cpp" "/home/osboxes/Desktop/CSCE-313/Projects/PA4/cmake-build-debug/CMakeFiles/PA4.dir/Histogram.cpp.o"
+  "/home/osboxes/Desktop/CSCE-313/Projects/PA4/client.cpp" "/home/osboxes/Desktop/CSCE-313/Projects/PA4/cmake-build-debug/CMakeFiles/PA4.dir/client.cpp.o"
+  "/home/osboxes/Desktop/CSCE-313/Projects/PA4/common.cpp" "/home/osboxes/Desktop/CSCE-313/Projects/PA4/cmake-build-debug/CMakeFiles/PA4.dir/common.cpp.o"
+  "/home/osboxes/Desktop/CSCE-313/Projects/PA4/server.cpp" "/home/osboxes/Desktop/CSCE-313/Projects/PA4/cmake-build-debug/CMakeFiles/PA4.dir/server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
