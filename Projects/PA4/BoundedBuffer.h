282adcb64342a8df7523354084b5dce2bfd78d17
osboxes.org <jeffreyxu@tamu.edu>
2020-10-13 18:37:28 -0400

    Bounded Buffer complete

--- Projects/PA4/BoundedBuffer.h
+++ Projects/PA4/BoundedBuffer.h
@@ -7,6 +7,7 @@
 #include <pthread.h>
 #include <mutex>
 #include <assert.h>
+#include <condition_variable>
 
 using namespace std;
 
@@ -24,6 +25,8 @@ private:
 
     // Thread-safety
     mutex m;
+    condition_variable data_available;
+    condition_variable slot_available;
 
 public:
     BoundedBuffer(int _cap){
@@ -35,33 +38,37 @@ public:
 
     void push(char* data, int len){
         //1. Wait until there is room in the queue (i.e., queue lengh is less than cap)
+        unique_lock<mutex> l(m);
+        slot_available.wait(l, [this]{return q.size() < cap;});
 
         //2. Convert the incoming byte sequence given by data and len into a vector<char>
         vector<char> d(data, data + len);
 
         //3. Then push the vector at the end of the queue
-        m.lock();
         q.push(d);
-        m.unlock();
+        l.unlock();
 
         //4. Wake up consumer threads(pop() threads)
+        data_available.notify_one();
 
     }
 
     int pop(char* buf, int bufcap){
         //1. Wait until the queue has at least 1 item
+        unique_lock<mutex> l(m);
+        data_available.wait(l, [this]{return q.size() > 0;});
 
         //2. pop the front item of the queue. The popped item is a vector<char>
-        m.lock();
         vector<char> d = q.front();
         q.pop();
-        m.unlock();
+        l.unlock();
 
         //3. Convert the popped vector<char> into a char*, copy that into buf, make sure that vector<char>'s length is <= bufcap
         assert(d.size() <= bufcap);
         memcpy(buf, d.data(), d.size());
 
         //4. Wake up producer threads(push() threads)
+        slot_available.notify_one();
 
         //5. Return the vector's length to the caller so that he knows many bytes were popped
         return d.size();
